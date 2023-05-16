75941f22c1c80c329f41748fcdfe7265da6c14cd
Filmon-gh <>
2023-05-16 13:59:51 +0000

    Update variable name for storing weather data from API response

--- run.py
+++ run.py
@@ -8,15 +8,17 @@ from datetime import datetime  # Import the datetime module for working with dat
 
 
 API_key="6112a97c554eb760c42d9012cfc32ad6"
-city_name="Amsterdam"
+city_name=input("Enter your city: ")
 # Construct the URL with API key and location
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}"
 # Make the API request
 response = requests.get(url)
+weather_data=response.json
 print(response)
+print(weather_data)
+
 
 
 
     
 
-  
