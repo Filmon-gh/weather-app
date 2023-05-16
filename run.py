595540d81a08aa69b08269eb394e9c11375f7c50
Filmon-gh <>
2023-05-16 13:18:11 +0000

    Add weather API request  with given API key and city name

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 # Import the required modules
+import requests  # Import the requests module for making HTTP requests
+import json  # Import the json module for working with JSON data
 from tkinter import *  # Import the tkinter module for GUI
 from datetime import datetime  # Import the datetime module for working with dates and times
-import json  # Import the json module for working with JSON data
-import requests  # Import the requests module for making HTTP requests
 
+
+
+
+API_key="6112a97c554eb760c42d9012cfc32ad6"
+city_name="Amsterdam"
+# Construct the URL with API key and location
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}"
+# Make the API request
+response = requests.get(url)
+print(response)
+
+
+
+    
+
+  
